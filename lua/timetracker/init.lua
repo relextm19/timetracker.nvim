bf77e3c59c51f9096d23833a2298b9bcccf92a2d
Mateusz Kielb <relextm19@gmail.com>
2026-02-19 19:10:37 +0100

    writing to databse

--- lua/timetracker/init.lua
+++ lua/timetracker/init.lua
@@ -1,6 +1,32 @@
-local helper = require('timetracker.helper')
+local sqlite = require("sqlite")
 
 local M = {}
+
+local start_times = {}
+
+local db = sqlite({
+    uri = vim.fs.normalize(vim.fn.stdpath("data") .. '/timetracker.db'),
+    sessions = {
+        ID          = true,
+        FileName    = "text",
+        ProjectName = "text",
+        StartTime   = "integer",
+        StartDate   = "text",
+        EndTime     = "integer",
+        EndDate     = "text",
+    }
+})
+
+local function newSession(file_name, project_name, start_time, start_date, end_time, end_date)
+    return {
+        FileName    = file_name,
+        ProjectName = project_name,
+        StartTime   = start_time,
+        StartDate   = start_date,
+        EndTime     = end_time,
+        EndDate     = end_date,
+    }
+end
 
 M.setup = function()
     local group = vim.api.nvim_create_augroup("TimeTrackerGroup", { clear = true })
@@ -8,9 +34,33 @@ M.setup = function()
     vim.api.nvim_create_autocmd("BufEnter", {
         group = group,
         callback = function(event)
-            print("Entered buffer: ", helper.getFileNameFromPath(event.file))
+            start_times[event.buf] = os.time()
         end,
     })
+
+    vim.api.nvim_create_autocmd("BufLeave", {
+        group = group,
+        callback = function(event)
+            local start_time = start_times[event.buf]
+            if start_time then
+                local end_time = os.time()
+                local time_spent = end_time - start_time
+                local file_path = vim.api.nvim_buf_get_name(event.buf)
+                local file_name = vim.fn.fnamemodify(file_path, ":t")
+                if time_spent > 0 and (file_name and file_name ~= "") then
+                    local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+                    local start_date = os.date("%Y-%m-%d", start_time)
+                    local end_date = os.date("%Y-%m-%d", end_time)
+
+                    local session = newSession(file_name, project_name, start_time, start_date, end_time, end_date)
+                    db.sessions:insert(session)
+                end
+
+                start_times[event.buf] = nil
+            end
+        end
+    })
 end
+
 
 return M
---
+++ make.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS Sessions(
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    FileName TEXT,
+    ProjectName TEXT,
+    StartTime INTEGER,
+    StartDate TEXT,
+    EndTime INTEGER,
+    EndDate TEXT
+)
